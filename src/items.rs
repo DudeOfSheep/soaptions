4b82b4caa89ba6803f123bcb93a8515ad88e203d
Mudshoes <kg082108@wgcloud.org>
2025-09-06 23:15:46 -0500

    Separated out item code to new "item" module.

---
+++ src/items.rs
@@ -0,0 +1,50 @@
+mod items {
+    enum Effect {
+        Rejuvenating(i8),
+        Toxic(i8),
+        Luck(i8),
+        Alacrity(i8),
+        Dexterity(i8),
+        Charm(i8),
+        Bland,
+    }
+
+    struct Ingredient<'a, F: FnMut(&mut Potion)> {
+        effect: Effect,
+        alter: F,
+        title: &'a str,
+        adj: &'a str,
+        stock: u32,
+    }
+
+    struct Potion<'e> {
+        value: i32,
+        tags: [&'e Effect; 5],
+        title: String,
+    }
+
+    impl<'e> Potion<'e> {
+        pub fn from<F>(mix: [&'e Ingredient<F>; 5]) -> Potion<'e>
+        where
+            F: FnMut(&mut Potion),
+        {
+            let mut tags: [&'e Effect; 5] = [&Effect::Bland; 5]; // Has to be initialized. ugh.
+            let mut title = String::new();
+            let mut value = 0;
+            mix.iter().enumerate().map(|(i, item)| {
+                tags[i] = &item.effect;
+                title += &item.adj;
+                value += 1;
+            });
+
+            Potion { value, tags, title }
+        }
+
+        pub fn new<F>(value: i32, tags: [&'e Effect; 5], title: String) -> Potion<'e>
+        where
+            F: FnMut(&mut Potion),
+        {
+            Potion { value, tags, title }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,5 @@
+pub mod items;
+
 fn main() {
-    let health_potion = Potion::new(mix);
     println!("Hello, world!");
-}
-
-enum Effect {
-    Rejuvenating(i8),
-    Toxic(i8),
-    Luck(i8),
-    Alacrity(i8),
-    Dexterity(i8),
-    Charm(i8),
-    Bland,
-}
-struct Potion<'e> {
-    value: i32,
-    tags: [&'e Effect; 5],
-    title: String,
-}
-
-impl<'e> Potion<'e> {
-    pub fn from<F>(mix: [&'e Ingredient<F>; 5]) -> Potion<'e>
-    where
-        F: FnMut(&mut Potion),
-    {
-        let mut tags: [&'e Effect; 5] = [&Effect::Bland; 5]; // Has to be initialized. ugh.
-        let mut title = String::new();
-        let mut value = 0;
-        mix.iter().enumerate().map(|(i, item)| {
-            tags[i] = &item.effect;
-            title += item.adj;
-            value += 1;
-        });
-
-        Potion { value, tags, title }
-    }
-
-    pub fn new<F>(value: i32, tags: [&'e Effect; 5], title: String) -> Potion<'e>
-    where
-        F: FnMut(&mut Potion),
-    {
-        Potion { value, tags, title }
-    }
-}
-
-struct Ingredient<'a, F: FnMut(&mut Potion)> {
-    effect: Effect,
-    alter: F,
-    title: &'a str,
-    adj: &'a str,
-    stock: u32,
 }
