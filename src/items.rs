a5c8e29e16d6f297e1a9272bd85a194a67ba8fee
Mudshoes <kg082108@wgcloud.org>
2025-09-07 01:00:11 -0500

    save

--- src/items.rs
+++ src/items.rs
@@ -17,6 +17,28 @@ mod items {
         stock: u32,
     }
 
+    impl<'a, F: FnMut(&mut Potion)> Ingredient<'a, F> {
+        pub fn new(effect: Effect, alter: F, title: &'a str, adj: &'a str) -> Ingredient<'a, F> {
+            Ingredient {
+                effect,
+                alter,
+                title,
+                adj,
+                stock: 0,
+            }
+        }
+
+        /// Manual assignment for an Ingredient's stock value
+        pub fn set_stock(&mut self, stock: u32) {
+            self.stock = stock;
+        }
+
+        /// Updates the ingredients stock by the ingredients stock equation
+        pub fn drop_stock(&mut self) {
+            self.stock -= 1;
+        }
+    }
+
     struct Potion<'e> {
         value: i32,
         tags: [&'e Effect; 5],
@@ -24,18 +46,21 @@ mod items {
     }
 
     impl<'e> Potion<'e> {
-        pub fn from<F>(mix: [&'e Ingredient<F>; 5]) -> Potion<'e>
+        pub fn from<F>(mix: [Option<&'e Ingredient<F>>; 5]) -> Potion<'e>
         where
             F: FnMut(&mut Potion),
         {
             let mut tags: [&'e Effect; 5] = [&Effect::Bland; 5]; // Has to be initialized. ugh.
             let mut title = String::new();
             let mut value = 0;
-            mix.iter().enumerate().map(|(i, item)| {
-                tags[i] = &item.effect;
-                title += &item.adj;
-                value += 1;
-            });
+
+            for (i, item) in mix.iter().enumerate() {
+                if let Some(n) = item {
+                    tags[i] = &n.effect;
+                    title += &n.adj;
+                    value += 1;
+                }
+            }
 
             Potion { value, tags, title }
         }
@@ -46,5 +71,13 @@ mod items {
         {
             Potion { value, tags, title }
         }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn potion_mixing() {}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 pub mod items;
 
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("It's Potion time!");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Issue with input");
+
+    match input.parse::<u32>().expect("Bad input, must be numerical.") {
+        _ => (),
+    }
+
+    Ok(())
 }
